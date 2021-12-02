f74a9ae1ef1f5b028034475fddc5b6e1e5905671
Andrew Davisson <akdavisson4@gmail.com>
2021-12-02 08:13:57 -0700

    count depth increases

---
+++ day1.py
@@ -0,0 +1,15 @@
+
+file_name = "day1-inputs.txt"
+with open(file_name) as f:
+  lines = f.readlines()
+
+increased_depth_count = 0
+
+i = 0
+while i < len(lines):
+  if i > 0:
+    if lines[i].strip() > lines[i-1].strip():
+      increased_depth_count += 1
+  i += 1
+
+print(increased_depth_count)
