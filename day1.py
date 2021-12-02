5f64f23a49d86d30b8f270eb5a476facc3d3beb8
Andrew Davisson <akdavisson4@gmail.com>
2021-12-02 09:17:58 -0700

    cleaned up and functional

--- day1.py
+++ day1.py
@@ -1,15 +1,44 @@
-
 file_name = "day1-inputs.txt"
-with open(file_name) as f:
-  lines = f.readlines()
 
-increased_depth_count = 0
+# Get formatted inputs from file
+def get_formatted_inputs(file_name):
+    with open(file_name) as f:
+        lines = f.readlines()
 
-i = 0
-while i < len(lines):
-  if i > 0:
-    if int(lines[i].strip()) > int(lines[i-1].strip()):
-      increased_depth_count += 1
-  i += 1
+    formatted_lines = []
+    for line in lines:
+        formatted_lines.append(int(line.strip()))
+  
+    return formatted_lines
 
-print(increased_depth_count)
+
+# count how many times depth increased
+def count_increase(values):
+    increased_depth_count = 0
+
+    i = 0
+    while i < len(values):
+        if i > 0:
+            if values[i] > values[i-1]:
+                increased_depth_count += 1
+        i += 1
+  
+    return increased_depth_count
+
+
+# get list of sums
+def get_sums(values):
+    sums = []
+
+    i = 0
+    while i < len(values)-1:
+        if i > 0:
+            sums.append(values[i-1] + values[i] + values[i+1]) 
+        i += 1
+
+    return sums
+
+
+print("increased depth count: " + str(count_increase(get_formatted_inputs(file_name))))
+print("increased depths of sums: " + str(count_increase(get_sums(get_formatted_inputs(file_name)))))
+
