897ab28163630e0be1106d7775e772041b6c2a3d
Andrew Davisson <akdavisson4@gmail.com>
2021-12-03 11:15:11 -0700

    refactored loops

--- day1.py
+++ day1.py
@@ -1,6 +1,8 @@
 file_name = "day1-inputs.txt"
 
 # Get formatted inputs from file
+
+
 def get_formatted_inputs(file_name):
     with open(file_name) as f:
         lines = f.readlines()
@@ -12,13 +14,11 @@ def get_formatted_inputs(file_name):
 def count_increase(values):
     increased_depth_count = 0
 
-    i = 0
-    while i < len(values):
+    for i in range(0, len(values)):
         if i > 0:
             if values[i] > values[i-1]:
                 increased_depth_count += 1
-        i += 1
-  
+
     return increased_depth_count
 
 
@@ -26,15 +26,14 @@ def count_increase(values):
 def get_sums(values):
     sums = []
 
-    i = 0
-    while i < len(values)-1:
+    for i in range(0, len(values)-1):
         if i > 0:
-            sums.append(values[i-1] + values[i] + values[i+1]) 
-        i += 1
+            sums.append(values[i-1] + values[i] + values[i+1])
 
     return sums
 
 
-print("increased depth count: " + str(count_increase(get_formatted_inputs(file_name))))
-print("increased depths of sums: " + str(count_increase(get_sums(get_formatted_inputs(file_name)))))
-
+print("increased depth count: " +
+      str(count_increase(get_formatted_inputs(file_name))))
+print("increased depths of sums: " +
+      str(count_increase(get_sums(get_formatted_inputs(file_name)))))
